cebe904c35fba2960c7d3b67df9c5eff82200e3a
CodeSevenic <sevenicbookings@gmail.com>
2022-11-29 13:03:41 +0200

    JS Layout Animation update

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-﻿
+﻿const links = document.querySelectorAll('.nav-item a');
+const activeNav = document.querySelector('.active-nav');
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    gsap.to(links, { color: '#252525' });
+  });
+});
